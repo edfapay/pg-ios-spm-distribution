49ae0aa9730d7a97b7462714568b384820018934
zik <a2zzuhaib@gmail.com>
2026-06-26 01:26:40 +0300

    debug release 1.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "edfapay_pg_sdk",
-            path: "edfapay_pg_sdk.release.xcframework.zip"
+            path: "edfapay_pg_sdk.debug.xcframework.zip"
         )
     ]
 )
